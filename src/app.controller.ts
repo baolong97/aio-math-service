76e401933368ea37240f92dfcb767db73e9bdf62
Long Pham <long29031997@gmail.com>
2023-03-03 21:23:09 +0700

    init

---
+++ src/app.controller.ts
@@ -0,0 +1,13 @@
+import { Controller } from '@nestjs/common';
+import { MessagePattern } from '@nestjs/microservices';
+import { AppService } from './app.service';
+
+@Controller()
+export class AppController {
+  constructor(private readonly appService: AppService) {}
+
+  @MessagePattern({ cmd: 'sum' })
+  accumulate(data: number[]): number {
+    return (data || []).reduce((a, b) => a + b);
+  }
+}
